cafdb7a3182bf2619aa9198c172db5ff0eb59ab0
Darlington112 <Darlingtonevelyn112@gmail.com>
2023-07-13 09:12:20 +0100

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const hamBurger = document.getElementById('navBar');
+const iconTag = document.getElementById('icon')
+
+iconTag.onclick = function () {
+    document.body.classList.toggle('dark-theme');
+    if (document.body.classList.contains('dark-theme')) {
+        iconTag.src = "./images/sun.png";
+    } else{
+        iconTag.src = "./images/moon.png";
+    }
+}
 
 
 if (bar) {
